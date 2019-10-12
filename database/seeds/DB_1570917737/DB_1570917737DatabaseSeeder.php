e5de4019c882054867d6072d614d0864025ba379
Gustavo Quintana <gjquintanap@gmail.com>
2019-10-12 18:57:08 -0400

    Organiza seeds por dependencia

--- database/seeds/DB_1570917737/DB_1570917737DatabaseSeeder.php
+++ database/seeds/DB_1570917737/DB_1570917737DatabaseSeeder.php
@@ -12,18 +12,23 @@ class DB_1570917737DatabaseSeeder extends Seeder
     public function run()
     {
         //
-        $this->call(DB_1570917737DataRowsTableSeeder::class);
         $this->call(DB_1570917737DataTypesTableSeeder::class);
-        $this->call(DB_1570917737MenuItemsTableSeeder::class);
+        $this->call(DB_1570917737DataRowsTableSeeder::class);
+
         $this->call(DB_1570917737MenusTableSeeder::class);
-        $this->call(DB_1570917737ObrasTableSeeder::class);
-        $this->call(DB_1570917737PermissionRoleTableSeeder::class);
-        $this->call(DB_1570917737PermissionsTableSeeder::class);
-        $this->call(DB_1570917737PresentacionesTableSeeder::class);
+        $this->call(DB_1570917737MenuItemsTableSeeder::class);
+
         $this->call(DB_1570917737RolesTableSeeder::class);
+        $this->call(DB_1570917737PermissionsTableSeeder::class);
+        $this->call(DB_1570917737PermissionRoleTableSeeder::class);
+
         $this->call(DB_1570917737SettingsTableSeeder::class);
-        $this->call(DB_1570917737TeatrosTableSeeder::class);
         $this->call(DB_1570917737TranslationsTableSeeder::class);
         $this->call(DB_1570917737UsersTableSeeder::class);
+
+        $this->call(DB_1570917737TeatrosTableSeeder::class);
+        $this->call(DB_1570917737ObrasTableSeeder::class);
+        $this->call(DB_1570917737PresentacionesTableSeeder::class);
+
     }
 }
