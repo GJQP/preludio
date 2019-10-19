3e3fac7789221d0437d4a54e6dde2dc73d6f1be1
Gustavo Quintana <gjquintanap@gmail.com>
2019-10-19 18:30:05 -0400

    Agrega modelos faltantes de migraciones

---
+++ app/Obra.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Obra extends Model
+{
+    const CREATED_AT = 'fecha_creacion';
+    const UPDATED_AT = 'fecha_modificacion';
+}
