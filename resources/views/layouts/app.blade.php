68a2e6779100c9f642cd9e50a51dd1d5c2c30296
Madot <migueldeolim1@gmail.com>
2019-10-19 13:02:29 -0400

    Inicializando template
    +Welcome Page usada como index, 2 secciones
    +Archivos img, js y css de estilos, NO todos cargados en webpack.mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,17 +11,49 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/all.js') }}" defer></script>
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
+
+    
 </head>
 <body>
+    <!-- Page Preloder -->
+	<div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <!-- Header section start -->   
+	    <header class="header-area">
+            <a href="home.html" class="logo-area">
+                <img src="img/logo.png" alt="">
+            </a>
+            <div class="nav-switch">
+                <i class="fa fa-bars"></i>
+            </div>
+            {{-- <div class="phone-number">+675 334 567 223</div> --}}
+            <nav class="nav-menu">
+                <ul>
+                    <li class="active"><a href="home.html">Home</a></li>
+                    <li><a href="about.html">About us</a></li>
+                    <li><a href="service.html">Services</a></li>
+                    <li><a href="portfolio.html">Portfolio</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        <!-- Header section end -->   
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -70,11 +102,69 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav> --}}
 
-        <main class="py-4">
+        <div class="containter">
             @yield('content')
-        </main>
+        </div>
+
+        <!-- Footer section start -->
+	<footer class="footer-section">
+            <div class="footer-social">
+                <div class="social-links">
+                    <a href="#"><i class="fa fa-pinterest"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9 offset-lg-3">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="footer-item">
+                                    <ul>
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">About us</a></li>
+                                        <li><a href="#">Services</a></li>
+                                        <li><a href="#">Portfolio</a></li>
+                                        <li><a href="#">Blog</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="footer-item">
+                                    <ul>
+                                        <li><a href="#">Terms & Conditions</a></li>
+                                        <li><a href="#">FAQ</a></li>
+                                        <li><a href="#">Help Desk</a></li>
+                                        <li><a href="#">Job Aplications</a></li>
+                                        <li><a href="#">Site Map</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="footer-item">
+                                    <ul>
+                                        <li><a href="#">Privacy</a></li>
+                                        <li><a href="#">Contact us</a></li>
+                                        <li><a href="#">Newsletter</a></li>
+                                        <li><a href="#">Clients Testimonials</a></li>
+                                        <li><a href="#">FAQ</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+         
+    </footer>
+    <!-- Footer section end -->
+    
     </div>
 </body>
 </html>
