5d016f2c9c3922050900a0691b7f4e0746ef9b86
Madot <migueldeolim1@gmail.com>
2019-10-19 13:02:29 -0400

    Inicializando template
    +Welcome Page usada como index, 2 secciones
    +Archivos img, js y css de estilos, NO todos cargados en webpack.mix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -98,3 +98,51 @@
         </div>
     </body>
 </html>
+ --}}
+
+ @extends('layouts.app')
+
+@section('content')
+<!-- Intro section start -->
+<section class="intro-section pt100 pb50">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 intro-text mb-5 mb-lg-0">
+					<h2 class="sp-title">We are a creative Architecture<span>Studio</span></h2>
+					<p>Pellentesque lorem dolor, malesuada eget tortor vitae, tristique lacinia lectus. Pellentesque sed accumsan risus, id aliquam nulla. Integer lorem risus, feugiat at mauris malesuada, accumsan pellentesque ipsum. Nunc dapibus, libero ut pulvinar accumsan, tortor nisl iaculis ligula. Curabitur finibus dolor vel lectus pretium interdum a eget ante. Morbi rhoncus feugiat imperdiet. Curabitur non maximus leo. Nulla in ipsum sed magna egestas bibendum. Integer in sem sagittis, commodo mi sit amet, commodo nibh. Suspendisse potenti. Aliquam erat volutpat. </p>
+					<a href="#" class="site-btn sb-dark">See Project</a>
+				</div>
+				<div class="col-lg-5 pt-4">
+					<img src="img/intro.png" alt="">
+				</div>
+			</div>
+		</div>
+	</section>
+    <!-- Intro section end -->
+    
+    	<!-- CTA section start -->
+	<section class="cta-section pt100 pb50">
+            <div class="cta-image-box"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7 pl-lg-0 offset-lg-5 cta-content">
+                        <h2 class="sp-title">Dare to dream of a modern <span>home</span></h2>
+                        <p>Pellentesque lorem dolor, malesuada eget tortor vitae, tristique lacinia lectus. Pellentesque sed accumsan risus, id aliquam nulla. Integer lorem risus, feugiat at mauris malesuada, accumsan pellentesque ipsum. Nunc dapibus, libero ut pulvinar accumsan, tortor nisl iaculis ligula. Curabitur finibus dolor vel lectus pretium interdum a eget ante. </p>
+                        <div class="cta-icons">
+                            <div class="cta-img-icon">
+                                <img src="img/icon/light/1.png" alt="">
+                            </div>
+                            <div class="cta-img-icon">
+                                <img src="img/icon/light/2.png" alt="">
+                            </div>
+                            <div class="cta-img-icon">
+                                <img src="img/icon/color/3.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- CTA section end -->
+    
+@endsection
