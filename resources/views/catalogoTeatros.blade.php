b614809ce62363e664abd065e1451f7be64f4be6
Madot <migueldeolim1@gmail.com>
2019-10-19 21:05:52 -0400

    Vista Catalogo Teatro y custom.css

--- resources/views/catalogoTeatros.blade.php
+++ resources/views/catalogoTeatros.blade.php
@@ -8,10 +8,21 @@
 <section class="intro-section pt100 pb50">
         <div class="container">
                 <div class="row">
-                    <div class="col-lg-7 intro-text mb-5 mb-lg-0"> 
+                    <div class="col-lg-7 intro-text"> 
                         <h2 class="sp-title">Teatros</h2> 
                     </div>
-                    
+                    <div class="flex-cont">
+                        @for ($i = 0; $i < 10; $i++)
+                            <div class="card text-center w33">
+                                <img src="img/cta-img.jpg" class="card-img-top" alt="Imagen del teatro">
+                                <div class="card-body">
+                                    <h6 class="card-title">NOMBRE DE TEATRO {{ $i }}</h6>
+                                    <a href="#" class="card-link stretched-link font-weight-light">Presentaciones</a> 
+                                </div>
+                            </div>
+                        @endfor
+                        
+                    </div>
                     
                 </div>
             </div>
