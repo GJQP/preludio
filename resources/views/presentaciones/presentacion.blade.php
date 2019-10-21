604c0e97396d9a1107d905bc27a808e8a9c92bff
robegon98 <ragonzalez1899@gmail.com>
2019-10-20 20:37:31 -0400

    Arreglos en la vista de detalle de teatro y presentacion
    agregar redes sociales a la pag de teatro y colocar las fotos del mismo tamaño

--- resources/views/presentaciones/presentacion.blade.php
+++ resources/views/presentaciones/presentacion.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('titulo')
-    Presentacion
+    Presentacion 
 @endsection
 
 @section('recursos')
@@ -13,88 +13,62 @@
 @endsection
 
 @section('contenido')
-<section class="intro-section pt100 pb-4">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-4 pt-4">
-                    <img src="/img/paseo-nueva.png" alt="">
-                </div>   
-                <div class="col-lg-7 mr-3 ml-3">
-                    <div class="sp-title mb-2"> 
-                         <h2> <span>Paseo las Mercedes</span> </h2> 
+<section class=" pt100 pb50">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <img src="/img/Los-Miserables.png" alt="" class="img-thumbnail">
+                </div>
+                <div class=" col-lg-8 ml-2">
+                    <div class="mb-2 sp-title"> 
+                        <h1> <span>Los Miserables</span></h1>
                     </div>
-                    <div> 
-                        <p><strong>Teléfono:</strong> 0424-1844392</p>
+                    <div>
+                            <a class="enlace" href="/teatro/1" target="_blank"> <h6>Paseo Las Mercedes</h6></a>
+                    </div>
+                    <div>
+                        <h6>Esto no seria lo mismo que nombre de presentacion?</h6>
+                    </div>
+                    <div>
+                        <h6>Desde el 31 de Octubre hasta el 3 de Noviembre</h6>
                     </div>
                     <div>
                         <blockquote>
-                            <p>ipsum dolor, sit amet consectetur adipisicing elit. Tempore aliquam minima eius porro magnam, numquam impedit cumque consectetur! Consectetur amet tenetur aperiam earum eius dicta ea dolorem quo incidunt itaque! </p>
+                            <p><strong> Sinopsis: </strong> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis similique autem nesciunt provident deleniti cum magnam eius totam quam. Nihil omnis aperiam praesentium doloribus rem velit alias facere, nam nesciunt.</p>
                         </blockquote>
                     </div>
-                </div>
-			</div>
-		</div>
-</section>
-<br>
-<section class="pt-3 pb-4">
-		<div class="container">
-            <div>
-                <h2 class="sub-line">Galería</h2>
-            </div>
-            <div class="row">
-                <div class="owl-carousel owl-theme">
-                        <div >
-                                <img src="/img/intro.png" alt="" class="img-thumbnail">
-                        </div>
-                            <div >
-                                <img src="/img/referencia.png" alt="" class="img-thumbnail">
-                            </div>
-                            <div >
-                                <img src="/img/intro.png" alt="" class="img-thumbnail">
-                            </div>
-                            <div >
-                                <img src="/img/trasnocho.png" alt="" class="img-thumbnail">
-                            </div>
-                            <div >
-                                <img src="/img/intro.png" alt="" class="img-thumbnail">
-                            </div>
-                            <div >
-                                <img src="/img/trasnocho2.png" alt="" class="img-thumbnail">
-                            </div>
-                </div>
-                
-            </div>    
-        </div>
-</section>
-<br>
-<section class="pt-4 pb-4">
-        <div class="container">
-                <div>
-                        <h2 class="sub-line">Presentaciones</h2>
-                </div>
-                <div class="row ">
-                    <div class="owl-carousel owl-theme">
-                            <div >
-                                    <img src="/img/Desesperados.png" alt="">
-                                </div>
-                                <div >
-                                    <img src="/img/encuentro.png" alt="">
-                                </div>
-                                <div >
-                                    <img src="/img/Los-Miserables.png" alt="">
-                                </div>
-                                <div >
-                                    <img src="/img/Teatro.png" alt="">
-                                </div>
-                                <div >
-                                    <img src="/img/poison.png" alt="">
-                                </div>
-                                <div >
-                                    <img src="/img/Conyuges.png" alt="">
-                                </div>
+                    <div>
+                        <p><strong>Reparto: </strong> Mariana Rodriguez, Tomás El Fakih, Gustavo Quintana, Miguel de Olim, Andrés de Armas, Robert González</p>
                     </div>
-                       
-                </div>     
+                </div>
+            </div>
         </div>
+<hr>
+        <div class="mt-3 container">
+                <div> 
+                    <h3 class="sub-line">Galería</h3>
+                </div>
+                <div>
+                    <div class=" row col-12 owl-carousel owl-theme">
+                        <div>
+                            <img src="/img/Desesperados.png" alt="" class="galeria" >
+                        </div>
+                        <div>
+                            <img src="/img/encuentro.png" alt="" class="galeria">
+                        </div>
+                        <div>
+                            <img src="/img/trasnocho.png" alt="" class="galeria">
+                        </div>
+                        <div>
+                            <img src="/img/Teatro.png" alt="" class="galeria">
+                        </div>
+                        <div>
+                            <img src="/img/poison.png" alt="" class="galeria">
+                        </div>
+                        <div>
+                            <img src="/img/Conyuges.png" alt="" class="galeria">
+                        </div>
+                    </div>   
+                </div>
+            </div>
 </section>
-@endsection
--- resources/views/teatros/teatro.blade.php
+++ resources/views/teatros/teatro.blade.php
@@ -21,7 +21,7 @@
                 </div>   
                 <div class="col-lg-7 mr-3 ml-3">
                     <div class="sp-title mb-2"> 
-                         <h2> <span>Paseo las Mercedes</span> </h2> 
+                         <h1> <span>Paseo las Mercedes</span> </h1>
                     </div>
                     <div> 
                         <p><strong>Teléfono:</strong> 0424-1844392</p>
@@ -31,9 +31,14 @@
                             <p>ipsum dolor, sit amet consectetur adipisicing elit. Tempore aliquam minima eius porro magnam, numquam impedit cumque consectetur! Consectetur amet tenetur aperiam earum eius dicta ea dolorem quo incidunt itaque! </p>
                         </blockquote>
                     </div>
+                    <div class="derecha mt-5 ">
+                        <a href="https://www.instagram.com/?hl=es-la" class=""><i class="fab fa-instagram rs" ></i></a>
+                        <a href="#"><i class="fab fa-facebook rs" style="font-size:30px"></i></a>
+                        <a href="#"><i class="fab fa-twitter rs" style="font-size:30px"></i></a>
+                    </div>
                 </div>
 			</div>
-		</div>
+		</div>  
 </section>
 <br>
 <section class="pt-3 pb-4">
@@ -43,23 +48,23 @@
             </div>
             <div class="row">
                 <div class="owl-carousel owl-theme">
-                        <div >
-                                <img src="/img/intro.png" alt="" class="img-thumbnail">
+                         <div >
+                                <img src="/img/intro.png" alt="" class="galeria">
                         </div>
                             <div >
-                                <img src="/img/referencia.png" alt="" class="img-thumbnail">
+                                <img src="/img/referencia.png" alt="" class="galeria">
                             </div>
                             <div >
-                                <img src="/img/intro.png" alt="" class="img-thumbnail">
+                                <img src="/img/intro.png" alt="" class="galeria">
                             </div>
                             <div >
-                                <img src="/img/trasnocho.png" alt="" class="img-thumbnail">
+                                <img src="/img/trasnocho.png" alt="" class="galeria">
                             </div>
                             <div >
-                                <img src="/img/intro.png" alt="" class="img-thumbnail">
+                                <img src="/img/intro.png" alt="" class="galeria">
                             </div>
                             <div >
-                                <img src="/img/trasnocho2.png" alt="" class="img-thumbnail">
+                                <img src="/img/trasnocho2.png" alt="" class="galeria">
                             </div>
                 </div>
                 
@@ -74,24 +79,24 @@
                 </div>
                 <div class="row ">
                     <div class="owl-carousel owl-theme">
-                            <div >
-                                    <img src="/img/Desesperados.png" alt="">
-                                </div>
-                                <div >
-                                    <img src="/img/encuentro.png" alt="">
-                                </div>
-                                <div >
-                                    <img src="/img/Los-Miserables.png" alt="">
-                                </div>
-                                <div >
-                                    <img src="/img/Teatro.png" alt="">
-                                </div>
-                                <div >
-                                    <img src="/img/poison.png" alt="">
-                                </div>
-                                <div >
-                                    <img src="/img/Conyuges.png" alt="">
-                                </div>
+                            <div>
+                                <a href="/presentacion/1"> <img src="/img/Desesperados.png" alt="" class="galeria"></a>
+                            </div>
+                            <div>
+                                <a href="/presentacion/1"> <img src="/img/encuentro.png" alt="" class="galeria"> </a>
+                            </div>
+                            <div>
+                                <a href="/presentacion/1"> <img src="/img/Los-Miserables.png" alt="" class="galeria"> </a>
+                            </div>
+                            <div>
+                                <a href="/presentacion/1">  <img src="/img/Teatro.png" alt="" class="galeria"></a>
+                            </div>
+                            <div>
+                                <a href="/presentacion/1"> <img src="/img/poison.png" alt="" class="galeria"> </a>
+                            </div>
+                            <div>
+                                <a href="/presentacion/1"> <img src="/img/Conyuges.png" alt="" class="galeria"> </a>
+                            </div>
                     </div>
                        
                 </div>     
